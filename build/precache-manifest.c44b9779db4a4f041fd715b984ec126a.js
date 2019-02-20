cc2bc3a55acb77588bb57ce7d8534f154144748c
Matt Basile <matthewbasile2@gmail.com>
2019-02-19 19:02:45 -0500

    restructing files for deployment

---
+++ build/precache-manifest.c44b9779db4a4f041fd715b984ec126a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a17a3dcd5bd1feec54d",
+    "url": "/static/css/main.11e50a3b.chunk.css"
+  },
+  {
+    "revision": "8a17a3dcd5bd1feec54d",
+    "url": "/static/js/main.8a17a3dc.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "81057c73703636e2afe6",
+    "url": "/static/js/2.81057c73.chunk.js"
+  },
+  {
+    "revision": "6048f90a78e9e52d51e28a3e6914ecd5",
+    "url": "/static/media/guidr_screen.6048f90a.png"
+  },
+  {
+    "revision": "714c4d085bd91d01ebe6b5c0172cb6df",
+    "url": "/static/media/pokedex_screen.714c4d08.png"
+  },
+  {
+    "revision": "4779e756928b8abe2016f8504284d00c",
+    "url": "/static/media/dbz_screen.4779e756.png"
+  },
+  {
+    "revision": "cb93b39b7f6165b0ed3c460e3fb8cea2",
+    "url": "/static/media/profile.cb93b39b.png"
+  },
+  {
+    "revision": "411cde0bbeb4f215c3dd4acda75bca38",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router'
+
+import HomePage from './components/HomePage/HomePage'
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Route exact path="/" component={HomePage} />
+      </div>
+    );
+  }
+}
+
+
+export default App;
