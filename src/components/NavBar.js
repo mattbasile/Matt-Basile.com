f05c8fba0ae488accdde1e47cd1fe18fc4eae4ef
Matt Basile <matthewbasile2@gmail.com>
2019-02-21 10:18:54 -0500

    optimzed for 5se

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-    <div className="flex w-full h-24 justify-around md:justify-end items-center">
+    <div className="nav-bar flex w-full h-24 justify-around md:justify-end items-center">
       <a className="mx-2 md:mx-4 text-xl text-black hover:underline no-underline font-bold" href="#profile">Profile</a>
       <a className="mx-2 md:mx-4 text-xl text-black hover:underline no-underline font-bold" href="#projects">Projects</a>
       <a className="mx-2 md:mx-4 text-xl text-black hover:underline no-underline font-bold" href="#articles">Articles</a>
