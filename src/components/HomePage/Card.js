dce873c9a802aeaa42e8d13233ad19e26981c5f6
Matt Basile <matthewbasile2@gmail.com>
2019-02-21 09:48:01 -0500

    optimized

--- src/components/HomePage/Card.js
+++ src/components/HomePage/Card.js
@@ -5,19 +5,19 @@ export default function Card(props) {
   const date = moment(props.card.pubDate).format('MM/DD/YYYY');
   return (
     <>
-       <div className="w-full flex">
-          <div className="w-1/4 h-64 flex justify-center items-center mb-8 left-article overflow-hidden">
+       <div className="w-full flex flex-wrap">
+          <div className="w-full md:w-1/4 h-auto md:h-64 flex justify-center items-center md:mb-8 left-article overflow-hidden">
             <img  src={props.card.thumbnail} alt=""/>
           </div>
   
-          <div className="w-3/4 right-article h-64 mb-8 flex justify-around flex-col">
+          <div className="w-full md:w-3/4 right-article h-auto md:h-64 mb-8 py-6 md:py-0 flex justify-around flex-col">
             <div>
-              <h3 className="text-3xl text-center main-font"><a className="text-black" href={props.card.guid} target="blank">{props.card.title}</a></h3>
+              <h3 className="text-xl md:text-3xl text-center main-font"><a className="text-black" href={props.card.guid} target="blank">{props.card.title}</a></h3>
               <h4 className="text-center mt-6 body-font">Published on: {date}</h4>
             </div>
               <div className="text-center">
               {props.card.categories.map((category, i) =>{
-                return <span key={i} className="my-1 body-font inline-block bg-blue-dark text-white px-3 py-2 border-4 border-black text-sm font-semibold mr-2">{category}</span>
+                return <span key={i} className="hidden my-1 body-font md:inline-block bg-blue-dark text-white px-3 py-2 border-4 border-black text-sm font-semibold mr-2">{category}</span>
               })}
               </div>
           </div>
--- src/components/HomePage/Contact.js
+++ src/components/HomePage/Contact.js
@@ -4,27 +4,27 @@ export default function Contact() {
   return (
     <div id="contact" className="w-4/5 mt-48 text-center mx-auto bg-pink py-6 h-auto flex flex-col justify-between">
      <div>
-      <h2 className="text-4xl main-font">Let's make something great together!</h2>
-      <div className="flex w-3/5 mx-auto justify-around">
+      <h2 className="text-2xl md:text-4xl main-font">Let's make something great together!</h2>
+      <div className="flex md:w-3/5 mx-auto justify-around">
         <a target="blank" href="mailto:matthewbasile2@gmail.com?Subject=Hey Matt! Just saw your Website...">
-          <i className="text-5xl my-8 far fa-envelope text-red-light hover:text-red "></i>
+          <i className="text-3xl md:text-5xl my-8 far fa-envelope text-red-light hover:text-red "></i>
         </a>
         <a target="blank" href="https://twitter.com/mattcbasile">
-        <i className="text-5xl my-8 text-blue-light hover:text-blue-dark  fab fa-twitter"></i>
+        <i className="text-3xl md:text-5xl my-8 text-blue-light hover:text-blue-dark  fab fa-twitter"></i>
         </a>
         <a target="blank" href="https://github.com/mattbasile">
-        <i className="text-5xl my-8 text-purple-dark  hover:text-purple-darker fab fa-github"></i>
+        <i className="text-3xl md:text-5xl my-8 text-purple-dark  hover:text-purple-darker fab fa-github"></i>
         </a>
         <a target="blank" href="https://www.linkedin.com/in/matthew-basile-070b279b/">
-        <i className="text-5xl my-8 text-blue-dark  hover:text-blue-darker fab fa-linkedin"></i>
+        <i className="text-3xl md:text-5xl my-8 text-blue-dark  hover:text-blue-darker fab fa-linkedin"></i>
         </a>
         <a target="blank" href="https://angel.co/matthew-basile">
-        <i className="text-5xl my-8 text-grey-dark hover:text-grey-darker fab fa-angellist"></i>
+        <i className="text-3xl md:text-5xl my-8 text-grey-dark hover:text-grey-darker fab fa-angellist"></i>
         </a>
       </div>
    </div> 
    <div className="body-font">
-      <h3  className="mt-12 mb-4">This site was made with <i className="heart fas fa-heart text-red-light"></i> by Matt Basile </h3>
+      <h3  className="mt-4 md:mt-12 mb-4">This site was made with <i className="heart fas fa-heart text-red-light"></i> by Matt Basile </h3>
       <p> {(new Date()).getFullYear()}  © Matt Basile. All Rights Reserved. </p>
    </div>
 </div>
